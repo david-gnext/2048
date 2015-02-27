2f0b57c6cd4204f4d67b5790280d623b6f512674
david.gnext <davidjawphan@gmail.com>
2015-02-27 10:31:19 +0630

    Update game alert
    for game alert

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -135,5 +135,5 @@ HTMLActuator.prototype.message = function (won) {
 HTMLActuator.prototype.clearMessage = function () {
   // IE only takes one value to remove at a time.
   this.messageContainer.classList.remove("game-won");
-  this.messageContainer.classList.remove("game-over");
+  this.messageContainer.classList.remove("game-won");
 };
